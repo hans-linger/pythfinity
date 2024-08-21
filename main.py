f8f0472a8af5a05d71ec7ef82fc114982700f0ca
hans-linger <hans@grandpa.digital>
2024-08-21 19:14:49 +0200

    -win

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from src.layout import make_layout
-from ctypes import windll
 
 from src.qtcore.main_window import MainWindow
-
-timeBeginPeriod = windll.winmm.timeBeginPeriod
-timeBeginPeriod(1)
-
-# window, pm = make_layout()
-# window.mainloop()
 
 app = QApplication(sys.argv)
 window = MainWindow()
